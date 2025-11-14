608d85446b2b965a61aa672eec5bea0ab65254a6
GSDV <sdvgab@gmail.com>
2025-11-13 19:44:53 -0600

    test1 changes

--- src/util/test/test1.ts
+++ src/util/test/test1.ts
@@ -7,8 +7,29 @@ export const test1: Test = {
     timeLimit: 1200,
     questions: [
         {
-            type: 'free-response',
-            question: 'This is a free response question'
+            type: "free-response",
+            question: "This is a free response question"
+        },
+        {
+            type: "multiple-choice",
+            question: "MC question",
+            options: ["answer 1", "answer 2", "answer 3", "answer 4"],
+            answer: 1
+        },
+        {
+            type: "select-all",
+            question: "select all question",
+            options: ["answer 1", "answer 2", "answer 3", "answer 4"],
+            answers: [1]
+        },
+        {
+            type: "sentence-completion",
+            question: "Complete the sentence: the fox ____ over the dog."
+        },
+        {
+            type: "true-false",
+            question: "True-False question.",
+            answer: "true"
         }
     ]
 };
